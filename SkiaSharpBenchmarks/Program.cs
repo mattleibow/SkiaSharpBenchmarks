c5c2d1c6e4b05e2559070a405b14e549baf837c4
Matthew Leibowitz <mattleibow@live.com>
2022-06-27 23:40:14 +0200

    clear it all out

--- SkiaSharpBenchmarks/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using BenchmarkDotNet.Running;
-
-namespace SkiaSharpBenchmarks
-{
-	public class Program
-	{
-		public static void Main(string[] args) => BenchmarkRunner.Run(typeof(Program).Assembly);
-	}
-}
